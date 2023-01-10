04c0f15cf8c0ec6dd0b371dce689512e357155dd
lapulpeta <lapulpeta>
2023-01-10 11:00:24 +0000

    Added web project. Moved core logic into separate project.

---
+++ Nostrid.Web/wwwroot/scripts.js
@@ -0,0 +1,34 @@
+﻿export function setTheme(theme) {
+    var attribute = "data-bs-theme";
+    if (theme!==null) {
+        document.body.setAttribute(attribute, theme);
+    }
+    return document.body.getAttribute(attribute);
+}
+
+export function hideOffcanvasMenu() {
+    if (!bootstrap) return;
+    var menu = document.getElementById("offcanvasMenu");
+    if (!menu) return;
+    var instance = bootstrap.Offcanvas.getInstance(menu);
+    if (!instance) return;
+    instance.hide();
+}
+
+export function showModal(element) {
+    if (!bootstrap) return;
+    var modal = bootstrap.Modal.getOrCreateInstance(element.firstChild);
+    if (!modal) return;
+    modal.show();
+}
+
+export function hideModal(element) {
+    if (!bootstrap) return;
+    var modal = bootstrap.Modal.getOrCreateInstance(element.firstChild);
+    if (!modal) return;
+    modal.hide();
+}
+
+export function copyToClipboard(text) {
+    navigator.clipboard.writeText(text);
+}
