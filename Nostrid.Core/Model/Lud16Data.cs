e29d28c6bc29e77db67996e57598a3cc3206f4b5
lapulpeta <lapulpeta>
2023-01-19 11:46:22 +0000

    Added Lud16 service

---
+++ Nostrid.Core/Model/Lud16Data.cs
@@ -0,0 +1,11 @@
+namespace Nostrid.Model;
+
+public class Lud16Data
+{
+    public string Callback { get; set; }
+
+    public long MinSendableMsat { get; set; }
+
+    public long MaxSendableMsat { get; set; }
+}
+
