75da487521b892b047d1ae3400dabc910c96efc7
lapulpeta <lapulpeta>
2023-01-23 08:47:36 +0000

    Added SingleEventSubscriptionFilter

---
+++ Nostrid.Core/Data/Relays/SingleEventSubscriptionFilter.cs
@@ -0,0 +1,26 @@
+using NNostr.Client;
+
+namespace Nostrid.Data.Relays;
+
+public class SingleEventSubscriptionFilter : SubscriptionFilter
+{
+    private readonly string id;
+
+    public SingleEventSubscriptionFilter(string id)
+    {
+        this.id = id;
+    }
+
+    public override NostrSubscriptionFilter[] GetFilters()
+    {
+        return new[] {
+            new NostrSubscriptionFilter() { Ids = new[]{id }, Kinds = new[]{ NostrKind.Text }, Limit = limitFilterData?.Limit, Since = limitFilterData?.Since, Until = limitFilterData?.Until },
+        };
+    }
+
+    public override SubscriptionFilter Clone()
+    {
+        return new SingleEventSubscriptionFilter(id);
+    }
+}
+
