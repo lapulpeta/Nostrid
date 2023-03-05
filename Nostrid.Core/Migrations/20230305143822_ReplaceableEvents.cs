48b94286766a369f2462e9e0919d32aef6075964
lapulpeta <lapulpeta>
2023-03-05 14:43:09 +0000

    Basic event replacement handling

---
+++ Nostrid.Core/Migrations/20230305143822_ReplaceableEvents.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nostrid.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReplaceableEvents : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ReplaceableId",
+                table: "Events",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_ReplaceableId",
+                table: "Events",
+                column: "ReplaceableId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_ReplaceableId_CreatedAt",
+                table: "Events",
+                columns: new[] { "ReplaceableId", "CreatedAt" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Events_ReplaceableId",
+                table: "Events");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Events_ReplaceableId_CreatedAt",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "ReplaceableId",
+                table: "Events");
+        }
+    }
+}
--- Nostrid.Core/Model/Extension/EventExtension.cs
+++ Nostrid.Core/Model/Extension/EventExtension.cs
@@ -1,5 +1,5 @@
+using Newtonsoft.Json.Linq;
 using NNostr.Client;
-using System.Collections.Concurrent;
 using System.Diagnostics;
 using System.Numerics;
 
@@ -7,164 +7,185 @@ namespace Nostrid.Model;
 
 public static class EventExtension
 {
-    public static Event FromNostrEvent(this NostrEvent ev)
-    {
-        var now = DateTimeOffset.UtcNow;
-        var ret = new Event()
-        {
-            Content = ev.Content,
-            CreatedAt = ev.CreatedAt?.UtcDateTime,
-            CreatedAtCurated = (!ev.CreatedAt.HasValue || ev.CreatedAt > now ? now : ev.CreatedAt.Value).ToUnixTimeSeconds(),
-            Deleted = ev.Deleted,
-            Id = ev.Id,
-            Kind = ev.Kind,
-            PublicKey = ev.PublicKey,
-            Signature = ev.Signature,
-            Tags = ev.Tags
-            .Select((t, i) =>
-                new TagData()
-                {
-                    TagIndex = i,
-                    Data0 = t.TagIdentifier,
-                    DataCount = t.Data.Count + 1,
-                    Data1 = t.Data.Count > 0 ? t.Data[0] : null,
-                    Data2 = t.Data.Count > 1 ? t.Data[1] : null,
-                    Data3 = t.Data.Count > 2 ? t.Data[2] : null,
-                })
-            .ToList(),
-            Difficulty = CalculateDifficulty(ev.Id),
-            HasPow = ev.Tags.Any(t => t.TagIdentifier == "nonce"),
-        };
-        ret.ReplyToId = GetReplyToId(ret);
-        ret.ReplyToRootId = GetReplyToRootId(ret);
-        ret.ChannelId = GetChannelId(ret);
-        ret.RepostEventId = GetRepostEventId(ret);
-        ret.DmToId = GetDmToId(ret);
+	public static Event ToEvent(this string json)
+	{
+		return JToken.Parse(json).ToObject<NostrEvent>()!.FromNostrEvent();
+	}
+
+	public static Event FromNostrEvent(this NostrEvent ev)
+	{
+		var now = DateTimeOffset.UtcNow;
+		var ret = new Event()
+		{
+			Content = ev.Content,
+			CreatedAt = ev.CreatedAt?.UtcDateTime,
+			CreatedAtCurated = (!ev.CreatedAt.HasValue || ev.CreatedAt > now ? now : ev.CreatedAt.Value).ToUnixTimeSeconds(),
+			Deleted = ev.Deleted,
+			Id = ev.Id,
+			Kind = ev.Kind,
+			PublicKey = ev.PublicKey,
+			Signature = ev.Signature,
+			Tags = ev.Tags
+			.Select((t, i) =>
+				new TagData()
+				{
+					TagIndex = i,
+					Data0 = t.TagIdentifier,
+					DataCount = t.Data.Count + 1,
+					Data1 = t.Data.Count > 0 ? t.Data[0] : null,
+					Data2 = t.Data.Count > 1 ? t.Data[1] : null,
+					Data3 = t.Data.Count > 2 ? t.Data[2] : null,
+				})
+			.ToList(),
+			Difficulty = CalculateDifficulty(ev.Id),
+			HasPow = ev.Tags.Any(t => t.TagIdentifier == "nonce"),
+		};
+		ret.ReplyToId = GetReplyToId(ret);
+		ret.ReplyToRootId = GetReplyToRootId(ret);
+		ret.ChannelId = GetChannelId(ret);
+		ret.RepostEventId = GetRepostEventId(ret);
+		ret.DmToId = GetDmToId(ret);
+		ret.ReplaceableId = GetReplaceableId(ret);
 		return ret;
-    }
+	}
 
-    public static NostrEvent ToNostrEvent(this Event ev)
-    {
-        return new NostrEvent()
-        {
-            Content = ev.Content,
-            CreatedAt = ev.CreatedAt,
-            Deleted = ev.Deleted,
-            Id = ev.Id,
-            Kind = ev.Kind,
-            PublicKey = ev.PublicKey,
-            Signature = ev.Signature,
-            Tags = ev.Tags
-                .Select(t =>
-                    new NostrEventTag()
-                    {
-                        TagIdentifier = t.Data0,
-                        Data = new[] { t.Data1, t.Data2, t.Data3 }.Take(t.DataCount - 1).ToList(),
-                    })
-                .ToList()
-        };
-    }
+	public static NostrEvent ToNostrEvent(this Event ev)
+	{
+		return new NostrEvent()
+		{
+			Content = ev.Content,
+			CreatedAt = ev.CreatedAt,
+			Deleted = ev.Deleted,
+			Id = ev.Id,
+			Kind = ev.Kind,
+			PublicKey = ev.PublicKey,
+			Signature = ev.Signature,
+			Tags = ev.Tags
+				.Select(t =>
+					new NostrEventTag()
+					{
+						TagIdentifier = t.Data0,
+						Data = new[] { t.Data1, t.Data2, t.Data3 }.Take(t.DataCount - 1).ToList(),
+					})
+				.ToList()
+		};
+	}
 
 
-    public static bool CheckPowTarget(this Event ev, bool failIfTargetMissing)
-    {
-        var nonce = ev.Tags.FirstOrDefault(t => t.Data0 == "nonce");
-        if (nonce == null)
-            return false;
+	public static bool CheckPowTarget(this Event ev, bool failIfTargetMissing)
+	{
+		var nonce = ev.Tags.FirstOrDefault(t => t.Data0 == "nonce");
+		if (nonce == null)
+			return false;
 
-        if (nonce.DataCount < 3)
-            return !failIfTargetMissing;
+		if (nonce.DataCount < 3)
+			return !failIfTargetMissing;
 
-        if (!int.TryParse(nonce.Data2, out var target))
-        {
-            return !failIfTargetMissing;
-        }
+		if (!int.TryParse(nonce.Data2, out var target))
+		{
+			return !failIfTargetMissing;
+		}
 
-        return ev.Difficulty >= target;
-    }
+		return ev.Difficulty >= target;
+	}
 
-    public static string? GetReplyToId(Event ev)
-    {
-        if (ev.Kind == NostrKind.Text || ev.Kind == NostrKind.ChannelMessage)
-        {
-            var preferred = ev.Tags
-                .Where(t => t.Data0 == "e" && t.Data3 == "reply")
-                .Select(t => t.Data1)
-                .FirstOrDefault();
-            if (preferred != null)
-            {
-                return preferred;
-            }
-            if (ev.Kind == NostrKind.ChannelMessage && ev.Tags.Count(t => t.Data0 == "e") <= 1)
-            {
-                return null;
-            }
-            return ev.Tags
-                .Where(t => t.Data0 == "e" && t.Data1 != null)
-                .Select(t => t.Data1)
-                .LastOrDefault();
-        }
-        else if (ev.Kind == NostrKind.DM)
-        {
+	public static string? GetReplyToId(Event ev)
+	{
+		if (ev.Kind == NostrKind.Text || ev.Kind == NostrKind.ChannelMessage)
+		{
+			var preferred = ev.Tags
+				.Where(t => t.Data0 == "e" && t.Data3 == "reply")
+				.Select(t => t.Data1)
+				.FirstOrDefault();
+			if (preferred != null)
+			{
+				return preferred;
+			}
+			if (ev.Kind == NostrKind.ChannelMessage && ev.Tags.Count(t => t.Data0 == "e") <= 1)
+			{
+				return null;
+			}
+			return ev.Tags
+				.Where(t => t.Data0 == "e" && t.Data1 != null)
+				.Select(t => t.Data1)
+				.LastOrDefault();
+		}
+		else if (ev.Kind == NostrKind.DM)
+		{
 			return ev.Tags
 				.Where(t => t.Data0 == "e" && t.Data1 != null)
 				.Select(t => t.Data1)
 				.FirstOrDefault();
 		}
-        return null;
-    }
+		return null;
+	}
 
-    public static string? GetReplyToRootId(Event ev)
-    {
-        if (ev.Kind == NostrKind.Text)
-        {
-            var preferred = ev.Tags
-                .Where(t => t.Data0 == "e" && t.Data3 == "root")
-                .Select(t => t.Data1)
-                .FirstOrDefault();
-            if (preferred != null)
-            {
-                return preferred;
-            }
-            return ev.Tags
-                .Where(t => t.Data0 == "e" && t.Data1 != null)
-                .Select(t => t.Data1)
-                .FirstOrDefault();
-        }
-        return null;
-    }
+	public static string? GetReplyToRootId(Event ev)
+	{
+		if (ev.Kind == NostrKind.Text)
+		{
+			var preferred = ev.Tags
+				.Where(t => t.Data0 == "e" && t.Data3 == "root")
+				.Select(t => t.Data1)
+				.FirstOrDefault();
+			if (preferred != null)
+			{
+				return preferred;
+			}
+			return ev.Tags
+				.Where(t => t.Data0 == "e" && t.Data1 != null)
+				.Select(t => t.Data1)
+				.FirstOrDefault();
+		}
+		return null;
+	}
 
-    public static string? GetRepostEventId(Event ev)
-    {
-        if (ev.Kind == NostrKind.Repost)
-        {
-            return ev.Tags
-                .Where(t => t.Data0 == "e" && t.Data1 != null)
-                .Select(t => t.Data1)
-                .FirstOrDefault();
-        }
-        return null;
-    }
+	public static string? GetRepostEventId(Event ev)
+	{
+		if (ev.Kind == NostrKind.Repost)
+		{
+			return ev.Tags
+				.Where(t => t.Data0 == "e" && t.Data1 != null)
+				.Select(t => t.Data1)
+				.FirstOrDefault();
+		}
+		return null;
+	}
 
-    public static string? GetChannelId(Event ev)
-    {
-        if (ev.Kind == NostrKind.ChannelMessage)
-        {
-            var preferred = ev.Tags
-                .Where(t => t.Data0 == "e" && t.Data3 == "root")
-                .Select(t => t.Data1)
-                .FirstOrDefault();
-            if (preferred != null)
-            {
-                return preferred;
-            }
-            return ev.Tags
-                .Where(t => t.Data0 == "e" && t.Data1 != null)
-                .Select(t => t.Data1)
-                .FirstOrDefault();
-        }
-        return null;
-    }
+	public static string? GetChannelId(Event ev)
+	{
+		if (ev.Kind == NostrKind.ChannelMessage)
+		{
+			var preferred = ev.Tags
+				.Where(t => t.Data0 == "e" && t.Data3 == "root")
+				.Select(t => t.Data1)
+				.FirstOrDefault();
+			if (preferred != null)
+			{
+				return preferred;
+			}
+			return ev.Tags
+				.Where(t => t.Data0 == "e" && t.Data1 != null)
+				.Select(t => t.Data1)
+				.FirstOrDefault();
+		}
+		return null;
+	}
+
+	// NIP-16: https://github.com/nostr-protocol/nips/blob/master/16.md
+	// NIP-33: https://github.com/nostr-protocol/nips/blob/master/33.md
+	public static string? GetReplaceableId(Event ev)
+	{
+		switch (ev.KindClass)
+		{
+			case NostrKindClass.Replaceable:
+				return $"{ev.Kind}:{ev.PublicKey}";
+			case NostrKindClass.ReplaceableParams:
+				string dstr = ev.Tags.Where(t => t.Data0 == "d").Select(t => t.Data1).FirstOrDefault() ?? string.Empty;
+				return $"{ev.Kind}:{ev.PublicKey}:{dstr}";
+		}
+		return null;
+	}
 
 	public static string? GetDmToId(Event ev)
 	{
@@ -179,46 +200,46 @@ public static class EventExtension
 	}
 
 	public static int CalculateDifficulty(string id)
-    {
-        var bytes = Convert.FromHexString(id);
-        Trace.Assert(bytes.Length == 32, "Id should be 256 bits long");
+	{
+		var bytes = Convert.FromHexString(id);
+		Trace.Assert(bytes.Length == 32, "Id should be 256 bits long");
 
-        int accumDiff = 0;
-        for (int i = 0; i < 32; i += 4)
-        {
-            var part = ((uint)bytes[i] << 24) + ((uint)bytes[i + 1] << 16) + ((uint)bytes[i + 2] << 8) + ((uint)bytes[i + 3]);
-            var diff = BitOperations.LeadingZeroCount(part);
-            Trace.Assert(diff <= 32, $"Diff is {diff}");
-            if (diff < 32)
-                return diff + accumDiff;
-            accumDiff += 32;
-        }
-        return 256; // Max
-    }
+		int accumDiff = 0;
+		for (int i = 0; i < 32; i += 4)
+		{
+			var part = ((uint)bytes[i] << 24) + ((uint)bytes[i + 1] << 16) + ((uint)bytes[i + 2] << 8) + ((uint)bytes[i + 3]);
+			var diff = BitOperations.LeadingZeroCount(part);
+			Trace.Assert(diff <= 32, $"Diff is {diff}");
+			if (diff < 32)
+				return diff + accumDiff;
+			accumDiff += 32;
+		}
+		return 256; // Max
+	}
 
-    public static bool Merge(List<NoteTree> list, IEnumerable<Event> source)
-    {
-        bool added = false;
-        foreach (var ev in source)
-        {
-            if (list.Exists(ev.Id))
-            {
-                continue;
-            }
+	public static bool Merge(List<NoteTree> list, IEnumerable<Event> source)
+	{
+		bool added = false;
+		foreach (var ev in source)
+		{
+			if (list.Exists(ev.Id))
+			{
+				continue;
+			}
 
-            int insertAt = list.Count;
-            for (int i = 0; i < list.Count; i++)
-            {
-                if (list[i].Note.CreatedAtCurated < ev.CreatedAtCurated)
-                {
-                    insertAt = i;
-                    break;
-                }
-            }
-            list.Insert(insertAt, new NoteTree(ev));
-            added = true;
-        }
-        return added;
-    }
+			int insertAt = list.Count;
+			for (int i = 0; i < list.Count; i++)
+			{
+				if (list[i].Note.CreatedAtCurated < ev.CreatedAtCurated)
+				{
+					insertAt = i;
+					break;
+				}
+			}
+			list.Insert(insertAt, new NoteTree(ev));
+			added = true;
+		}
+		return added;
+	}
 }
 
